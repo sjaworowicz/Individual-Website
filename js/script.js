8a608f36ae92a291d0bbd0005988692036c1c3af
sjaworowicz <125sjaworowicz@frhsd.com>
2024-04-30 08:21:32 -0400

    Finished the javascript, only wanna make a few more adjustments to the website

--- js/script.js
+++ js/script.js
@@ -1,20 +1,24 @@
-const paragraphChangeJS = document.getElementById("paragraphchange");
-function changeParagraph1 (paragraphchange){
-paragraphChangeJS.innerHTML = "The Mets have won 2 World Series Championships in the years 1969 and 1986."
+const paragraphChangeJS1 = document.getElementById("paragraphchange1");
+function changeParagraph1 (){
+paragraphChangeJS1.innerHTML = "The Mets have won 2 World Series Championships in the years 1969 and 1986."
 }
 
-function changeParagraph2 (paragraphchange){
-paragraphChangeJS.innerHTML = "The Mets have retired Willie Mays #24, Keith Hernandez #17, jerry Koosman #36, Mike Piazza #31, Jackie Robinson #42, Tom Seaver #41, Gil Hodges #14, and Casey Stengal #37. Some mentionable references here are Mets broadcasters Bob Murphy and Ralph Kiner, as well as Bill Shea."
+const paragraphChangeJS2 = document.getElementById("paragraphchange2");
+function changeParagraph2 (){
+paragraphChangeJS2.innerHTML = "The Mets have retired Willie Mays #24, Keith Hernandez #17, jerry Koosman #36, Mike Piazza #31, Jackie Robinson #42, Tom Seaver #41, Gil Hodges #14, and Casey Stengal #37. Some mentionable references here are Mets broadcasters Bob Murphy and Ralph Kiner, as well as Bill Shea."
 }
 
-function changeParagraph3 (paragraphchange){
-paragraphChangeJS.innerHTML = "The Mets have won 5 pennants in the years 1969, 1973, 1986, 2000, and 2015."
+const paragraphChangeJS3 = document.getElementById("paragraphchange3");
+function changeParagraph3 (){
+paragraphChangeJS3.innerHTML = "The Mets have won 5 pennants in the years 1969, 1973, 1986, 2000, and 2015."
 }
 
-function changeParagraph4 (paragraphchange){
-paragraphChangeJS.innerHTML = "In the mets hall of fame, there is Casey Stengel, Gil Hodges, George M. Weiss, William A. Shea, Johnny Murphy, Ralph Kiner, Bob Murphy, Lindsey Nelson, Bud Harrelson, Rusty Staub, Tom Seaver, Jerry Koosman, Ed Kranepool, Cleon Joseph Jones, Jerry Grote, Tug Mcgraw, Mookie Willson, Keith Hernandez, Gary Edmund Carter, Tommie L. Agee, Frank Cashen, Doc Gooden, Davey Johnson, Darryl Strawberry, John Anthony Franco, Mike Piazza, John Matlack, Ron Darling, Fonzie Alfonzo, Hojo Johnson, Howie Rose, Gary Cohen, Al Leiter, and Joan Whitney Payson."
+const paragraphChangeJS4 = document.getElementById("paragraphchange4");
+function changeParagraph4 (){
+paragraphChangeJS4.innerHTML = "In the mets hall of fame, there is Casey Stengel, Gil Hodges, George M. Weiss, William A. Shea, Johnny Murphy, Ralph Kiner, Bob Murphy, Lindsey Nelson, Bud Harrelson, Rusty Staub, Tom Seaver, Jerry Koosman, Ed Kranepool, Cleon Joseph Jones, Jerry Grote, Tug Mcgraw, Mookie Willson, Keith Hernandez, Gary Edmund Carter, Tommie L. Agee, Frank Cashen, Doc Gooden, Davey Johnson, Darryl Strawberry, John Anthony Franco, Mike Piazza, John Matlack, Ron Darling, Fonzie Alfonzo, Hojo Johnson, Howie Rose, Gary Cohen, Al Leiter, and Joan Whitney Payson."
 }
 
-function changeParagraph5 (paragraphchange){
-paragraphChangeJS.innerHTML = "During the lifespan of the mets, they have had some moments that will go down in history. Here are just some of those moments. In 1962, the New York Mets became a baseball team. Then, in October 1969, the Mets won their first pennant, and world series as an organization. Then in October 1973, the Mets upset the Reds, bringing back their second pennant in history, but lost to the Athletics 4 games to 3 in the World Series.Then in 2009, the Mets said goodbye to Shea Stadium, and hello to Citi Field, which is their home field now. On June 1, 2012, Johan Santana threw the Mets first no hitter in franchise history against the Cardinals, recording 8 strikeouts, 5 walks, and 134 pitches. In 2019, Pete Alonso set an MLB record for most home runs for a rookie, as well as receiving rookie of the year, and Jacob DeGrom won the Cy Young award for 2 straight years, becoming the 11th pitcher in MLB history to win the award in back to back seasons."
+const paragraphChangeJS5 = document.getElementById("paragraphchange5");
+function changeParagraph5 (){
+paragraphChangeJS5.innerHTML = "During the lifespan of the mets, they have had some moments that will go down in history. Here are just some of those moments. In 1962, the New York Mets became a baseball team. Then, in October 1969, the Mets won their first pennant, and world series as an organization. Then in October 1973, the Mets upset the Reds, bringing back their second pennant in history, but lost to the Athletics 4 games to 3 in the World Series.Then in 2009, the Mets said goodbye to Shea Stadium, and hello to Citi Field, which is their home field now. On June 1, 2012, Johan Santana threw the Mets first no hitter in franchise history against the Cardinals, recording 8 strikeouts, 5 walks, and 134 pitches. In 2019, Pete Alonso set an MLB record for most home runs for a rookie, as well as receiving rookie of the year, and Jacob DeGrom won the Cy Young award for 2 straight years, becoming the 11th pitcher in MLB history to win the award in back to back seasons."
 }
